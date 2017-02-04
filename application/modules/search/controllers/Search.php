110103653c4e7411ea2f9c6da5e1ee77487102ac
Ashutosh Jindal <ashujindal92@gmail.com>
2017-02-04 17:11:40 +0530

    changes

---
+++ application/modules/search/controllers/Search.php
@@ -0,0 +1,12 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+class Search extends MX_Controller{
+
+	public function __construct(){
+	}
+	public function index(){
+		echo "mice";
+	}
+
+}
+?>
