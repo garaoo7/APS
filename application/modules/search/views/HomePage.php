db5395e461929e63a94ad63ff30e372f0ccf64f7
praveen <praveen@praveen-system.(none)>
2017-02-06 13:28:41 +0530

    autoSuggester changes

--- application/modules/search/views/HomePage.php
+++ application/modules/search/views/HomePage.php
@@ -10,20 +10,24 @@
 			<h1 style="margin-left: 10px;color:#696969;font-family: arial, sans-serif;font-size:12px;">APS</h1>			
 		
 		</header>
-		<div>
-			
-			<form name="aps" action="#" method="GET"><br>
-				<input type="search" class="search" id="searchBox" placeholder="Search a question">
-				<input type="button" class="button" name="submit" value="Search">
+		
+		<div class="main-div">
+			<form name="aps" action="#" method="GET"><br>	
+				<div>
+					<input type="search" class="search" id="searchBox" placeholder="Search a question">
+					<input type="button" class="button" name="submit" value="Search">
+				</div>
+				<div id ="recommdated-questions" class="recommdated-questions"></div>
 			</form>
 		</div>
-		<footer>
-			<!-- <a class="leftlinks" href="https://www.google.com/intl/en/ads/?fg=1">Advertising</a>
+		
+		<!-- <footer>
+			<a class="leftlinks" href="https://www.google.com/intl/en/ads/?fg=1">Advertising</a>
 			<a class="leftlinks" href="https://www.google.com/services/?fg=1">Business</a>
 			<a class="leftlinks" href="https://www.google.com/intl/en/about/">About</a>
 			<a class="rightlinks" href="https://www.google.com/preferences?hl=en">Settings</a>
-			<a class="rightlinks" href="https://www.google.com/intl/en/policies/?fg=1">Privacy & Terms</a> -->
-		</footer>
+			<a class="rightlinks" href="https://www.google.com/intl/en/policies/?fg=1">Privacy & Terms</a>
+		</footer> -->
 	</body>
 	<script src="<?php echo base_url('js/jquery-3.1.1.min.js')?>"></script>
 	<script src="<?php echo base_url('js/autosuggestor.js')?>"></script>
--- js/autosuggestor.js
+++ js/autosuggestor.js
@@ -15,10 +15,24 @@ var autoSuggestorClass = function(){
 				url: base_url +'search/search/getSuggestion/',
 			})
 			.done(function(response) {
-				console.log(response);
+        self.prepareAutoSuggestor(response);
 	  		});	
     	}
 	}
+
+  this.prepareAutoSuggestor = function(response){
+    if(response != ''){
+        var recommendateQuestions = JSON.parse(response);
+        var recommendateQuestionsData = '';
+        for(var question in recommendateQuestions){
+          recommendateQuestionsData += "<li class ='recommdated-question' id='recommendateQuestions'><span>"+recommendateQuestions[question]+"</span></li>";
+        }
+        $("#recommdated-questions").css('display','block');
+        $("#recommdated-questions").html(recommendateQuestionsData);
+        apsObj.bindEvents('click','.recommdated-question');
+    }
+  }
+
 	this.processSearchTerm = function(searchTerm){
 		return searchTerm;
 	}
@@ -37,11 +51,15 @@ var apsSearchClass = function(obj){
   }
   
   this.bindEvents = function(eventName, elementSelector) {
-    $(document).on(eventName, elementSelector,function(event) {
+    $(elementSelector).on(eventName,function(event) {
       switch(elementSelector) {
         case '#searchBox':
           	autoSuggestionObj.getSuggestions($(this).val());
         break;
+
+        case '.recommdated-question':
+            alert('go to question detail page');
+            break;
       }
     });
   }
